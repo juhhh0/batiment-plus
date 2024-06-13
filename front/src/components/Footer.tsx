cb71f54cff66ea0a9c4e1c3a933104ec29fd561f
Joris Mansion <juhh@juhh-core.lan>
2024-06-13 16:03:25 +0200

    footer responsive

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -10,20 +10,21 @@ export default function Footer({
 }) {
   return (
     <footer className="bg-black text-white  py-10 px-5">
-      <div className="flex flex-col-reverse md:flex-row gap-6">
-        <div className="justify-center items-center flex-1 flex">
+      <div className="flex flex-col-reverse md:flex-row gap-6 text-center">
+        <div className="flex justify-center items-center flex-1">
           <img
             className="w-20 white-img"
             src={
-              process.env.NEXT_PUBLIC_STRAPI_URL + data.globals.logo.data.attributes.url
+              process.env.NEXT_PUBLIC_STRAPI_URL +
+              data.globals.logo.data.attributes.url
             }
             alt=""
           />
         </div>
-        <div className="flex-1 flex items-center md:justify-center">
+        <div className="flex-1 flex items-center justify-center">
           <Informations data={data.globals} />
         </div>
-        <div className="flex flex-1 md:justify-center">
+        <div className="flex flex-1 justify-center">
           <ul>
             <li>
               <Link href={"/"}>Accueil</Link>
@@ -33,13 +34,13 @@ export default function Footer({
                 <Link href={link.link}>{link.label}</Link>
               </li>
             ))}
+            <li>
+              <Link href="/mentions-legales">Mentions Légales</Link>
+            </li>
           </ul>
         </div>
       </div>
       <ul className="flex gap-4 justify-center mt-6">
-        <li>
-          <Link href="/mentions-legales">Mentions Légales</Link>
-        </li>
         <li>© 2024 - Bâtiment Plus</li>
       </ul>
     </footer>
