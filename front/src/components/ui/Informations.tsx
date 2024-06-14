d2355bfa36f9b9c09bb4957ec000bcc552fbc560
Joris Mansion <juhh@juhh-core.lan>
2024-06-14 18:16:15 +0200

    footer icons

--- front/src/components/ui/Informations.tsx
+++ front/src/components/ui/Informations.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 
-export default function Informations({data} : {data: any}) {
+export default function Informations({ data }: { data: any }) {
   return (
-    <ul className="flex flex-col">
-    <li>
-      <span className="font-semibold">Adresse : </span>
-      {data.adress}
-    </li>
-    <li>
-      <span className="font-semibold">Email : </span>
-      {data.email}
-    </li>
-    <li>
-      <span className="font-semibold">Téléphone : </span>
-      {data.phone}
-    </li>
-  </ul>
-  )
+    <ul className="informations flex flex-col items-center">
+      <Item icon="location.svg" label="Adresse">
+        {data.adress}
+      </Item>
+      <Item icon="mail.svg" label="Email">
+        <a href={"mailto:" + data.email}>{data.email}</a>
+      </Item>
+      <Item icon="phone.svg" label="Téléphone">
+        <a href={"tel:" + data.phone}>{data.phone}</a>
+      </Item>
+    </ul>
+  );
 }
+
+const Item = ({
+  icon,
+  label,
+  children,
+}: {
+  icon: string;
+  label: string;
+  children: any;
+}) => (
+  <li className="flex gap-1">
+    <img className="w-6" src={"/icons/" + icon} alt="" />
+    <span className="font-semibold">{label} : </span>
+    {children}
+  </li>
+);
